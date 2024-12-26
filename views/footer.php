892f43e61ea3e9cd9f44f8c5e68e735fc435b852
Rama <s1206152@student.windesheim.nl>
2024-12-26 15:41:39 +0100

    d2

--- views/footer.php
+++ views/footer.php
@@ -6,7 +6,6 @@
         <a href="https://www.linkedin.com/legal/professional-community-policies?openinweb=true"><img class="linked" src="https://banner2.cleanpng.com/20180417/ifw/avfn2u8al.webp" alt="LinkedIn"></a>
         <a href="#"><img class="insta" src="https://w7.pngwing.com/pngs/910/192/png-transparent-instagram-instagram-new-design-liner-round-social-media-instagram-new-icon.png" alt="Instagram"></a>
         <a href="https://www.tiktok.com/@roro.mari22?_t=8q5sxdJ7iMd&_r=1" target="_blank"><img class="tik" src="https://w7.pngwing.com/pngs/483/249/png-transparent-tiktok-icon-thumbnail.png" alt="TikTok"></a>
-        <a href="https://github.com/ramame2" target="_blank"><img class="github" src="/github.png" alt="Github"></a>
     </div>
 
     <?php if (basename($_SERVER['SCRIPT_NAME']) !== 'index.php'): ?>
